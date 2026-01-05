dea4e741c4cee19e767739dc13dc3ee3442e1dd4
JoungDongHee <kes9511@funin.camp>
2026-01-05 10:40:55 +0900

    firstCommit

---
+++ src/main/java/com/gittest/githubactiontest/Test/TestController.java
@@ -0,0 +1,17 @@
+package com.gittest.githubactiontest.Test;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test/{value}")
+    public String test(@PathVariable String value){
+
+        return "Hello World"+value;
+    }
+}
